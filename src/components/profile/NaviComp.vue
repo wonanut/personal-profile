72576f2d1f8bd71772ffaef3b354f39318581d31
哆啦A瑶 <1343121616@qq.com>
2022-04-10 13:42:28 +0800

    初始化项目

---
+++ src/components/profile/NaviComp.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </div>
+</template>
+
+<script type="typescript">
+export default {
+  name: 'NaviComp',
+  data() {
+    return {
+
+    };
+  },
+};
+</script>
+
+<style></style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,53 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="profile">
+    <main>
+      <aside class="profile-aside">
+        <div>1234rw</div>
+      </aside>
+      <div class="profile-main">
+        <div>main</div>
+      </div>
+    </main>
   </div>
 </template>
+
+<script>
+import '@/assets/css/common.css';
+
+export default {
+  name: 'ProfileView',
+};
+</script>
+
+<style scoped>
+.profile {
+  width: 100%;
+  height: 100%;
+  background: var(--smoky-black);
+}
+
+main {
+  margin: 15px 12px;
+  margin-bottom: 75px;
+  min-width: 259px;
+}
+
+.profile-aside {
+  position: sticky;
+  top: 60px;
+  max-height: max-content;
+  height: 100%;
+  margin-bottom: 0;
+  padding-top: 60px;
+  z-index: 1;
+}
+
+.profile-main {
+  min-width: 75%;
+  width: 75%;
+  margin: 0;
+}
+
+@media (min-width: 1250px) {
+}
+</style>
